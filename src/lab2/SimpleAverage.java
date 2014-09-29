85391b7e2abd99e9dd18927a439fa36a730a2020
John Tran <johnjtran@icloud.com>
2014-09-28 19:53:54 -0700

    add lab1 and lab2 examples

---
+++ src/lab2/SimpleAverage.java
@@ -0,0 +1,19 @@
+package lab2;
+
+public class SimpleAverage {
+
+	public static void main(String[] args) {
+		int a1 = (int)(Math.random() * 10) + 1;
+		int a2 = (int)(Math.random() * 10) + 1;
+		int a3 = (int)(Math.random() * 10) + 1;
+		int a4 = (int)(Math.random() * 10) + 1;
+		
+		double avg = ((a1 + a2 + a3 + a4)/(double)4);
+		
+		System.out.println("1st number is " + a1);
+		System.out.println("2nd number is " + a2);
+		System.out.println("3rd number is " + a3);
+		System.out.println("4th number is " + a4);
+		System.out.println("Average is " + avg);
+	}
+}
