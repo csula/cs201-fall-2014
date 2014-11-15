c5e9ff021617422a4312d87a33e21264a22c78d7
John Tran <johnjtran@icloud.com>
2014-11-15 15:16:06 -0800

    tweaks and cleanup

--- src/quiz2/TalesOfTwoCities.java
+++ src/quiz2/TalesOfTwoCities.java
@@ -2,26 +2,30 @@ package quiz2;
 
 import java.util.ArrayList;
 
+/*
+ * Note that you cannot change any data or method interface
+ * 
+ */
+
 class City {
-    private String name;
+    private String name = "undefined";
     private ArrayList<City> neighbors = new ArrayList<City>();
-    
+
     public City() {
-        name = "undefined";
     }
-    
+
     public City(String name) {
         this.name = name;
     }
-    
+
     public String toString() {
         return name;
     }
-    
+
     public ArrayList<City> getNeighbors() {
         return neighbors;
     }
-    
+
     public void addNeighbor(City neighbor) {
         neighbors.add(neighbor);
     }
@@ -32,24 +36,24 @@ class City {
 }
 
 class State {
-    String name;
-    ArrayList<City> cities = new ArrayList<City>();
-    
+    private String name = "undefined";
+    private ArrayList<City> cities = new ArrayList<City>();
+
     public State() {
         /* default constructor */
     }
-    
+
     public State(String name) {
         this.name = name;
     }
-    
+
     City findCity(String name) {
         /* 
          * write code to find the city of a state 
          * 5 points
          * 
          */
-        
+
         /* not found */
         return null;
     }
@@ -62,7 +66,7 @@ class State {
          */
         return null;
     }
-    
+
     public void addCity(City city) {
         /*
          * write code to add the city to a state
@@ -70,7 +74,7 @@ class State {
          * 
          */
     }
-    
+
     public void printCities() {
         /*
          * write code to print all the cities in a state
@@ -78,7 +82,7 @@ class State {
          * 
          */
     }
-    
+
     public boolean isNeighbor(State otherState) {
         /*
          * write code to test to see if the other state is a neighbor
@@ -100,16 +104,16 @@ public class TalesOfTwoCities {
         }
         System.out.println("");
     }
-    
+
     public static void main(String[] args) {
         City NewYorkCity = new City( "new york" );
         City Philadelphia = new City( "philadelphia" );
         City Boston = new City( "boston" );
-        
+
         cities.add( NewYorkCity );
         cities.add( Philadelphia );
         cities.add( Boston );
-        
+
         /* 
          * Define relationships
          * 
@@ -118,17 +122,17 @@ public class TalesOfTwoCities {
          * Boston is not a neighbor of Philadelphia
          * 
          */
-        
+
         NewYorkCity.addNeighbor( Boston );
         NewYorkCity.addNeighbor( Philadelphia );
         Boston.addNeighbor( NewYorkCity );
         Philadelphia.addNeighbor( NewYorkCity );
-        
+
         /* 
          * print out all of the cities 
          * 
          */
-        
+
         for (City city : cities) {
             System.out.println(city);
         }
@@ -137,31 +141,31 @@ public class TalesOfTwoCities {
          * print out all of the neighbors 
          * 
          */
-        
+
         printNeighbors( NewYorkCity );
         printNeighbors( Philadelphia );
         printNeighbors( Boston );
-        
+
         /*
          * State 
          * 
          */
-        
+
         State Pennsylvania = new State("pennsylvania");
         Pennsylvania.addCity( Philadelphia );
-        
+
         State NewYorkState = new State("new york");
         NewYorkState.addCity( NewYorkCity );
-        
+
         State Massachusett = new State("massaschusett");
         Massachusett.addCity( Boston );
-        
+
         /*
          * test to see if the two states are neighbors
          * a state is a neighbor if there they have one or more cities
          * that are neighbors
          */
-        
+
         System.out.println(Pennsylvania + " and " + NewYorkState + " are neighbors: " + Pennsylvania.isNeighbor( NewYorkState ));
         System.out.println(Pennsylvania + " and " + Massachusett + " are neighbors: " + Pennsylvania.isNeighbor( Massachusett ));
         System.out.println(NewYorkState + " and " + Massachusett + " are neighbors: " + NewYorkState.isNeighbor( Massachusett ));
